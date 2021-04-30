970b2448469e6e383791d80017f3037239aeeabb
Tatsuya Teshima <terima666tatsuya@gmail.com>
2021-04-30 19:51:34 +0900

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=0.12.20"
+}
+
+provider "aws" {
+  version = ">=2.63.0"
+  region = "us-east-2"
+}
+
+module "network" {
+  source = "./modules/aws/network"
+  vpc_cidr = "10.0.0.0/16"
+}
